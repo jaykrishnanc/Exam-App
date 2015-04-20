abd699b6c33cf3d71dde2701b2cdcbc407916bac
Jayakrishnan C <jaykrishnanc@gmail.com>
2015-04-20 22:54:27 +0530

    first

---
+++ app.js
@@ -0,0 +1,9 @@
+﻿var express = require('express');
+var app = express();
+app.use(express.static(__dirname));
+var appInfo = JSON.parse(process.env.VCAP_APPLICATION || "{}");
+var services = JSON.parse(process.env.VCAP_SERVICES || "{}");
+var host = (process.env.VCAP_APP_HOST || 'localhost');
+var port = (process.env.VCAP_APP_PORT || 3000);
+app.listen(port, host);
+console.log('App started on port ' + port);
